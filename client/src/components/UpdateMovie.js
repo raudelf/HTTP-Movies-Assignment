4603d0abbb601fdee4d336b92061f6020565e30d
Raudel Flores <rawdell@outlook.com>
2020-02-18 22:08:22 -0600

    created the form and functionality of it. Just need to work on the submit handler

--- client/src/components/UpdateMovie.js
+++ client/src/components/UpdateMovie.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const initialItem = {
     title: '',
@@ -8,12 +9,51 @@ const initialItem = {
 }
 
 const UpdateMovie = props => {
+    const [update, setUpdate] = useState(initialItem);
+
+    useEffect(() => {
+        const selectedMovie = props.movies.find(movie => {
+            return `${movie.id}` === props.match.params.id
+        });
+        console.log(selectedMovie);
+        if (selectedMovie) {
+            setUpdate(selectedMovie)
+        }
+    }, [props.movies, props.match.params.id]);
+
+    const handleChanges = e => {
+        setUpdate({
+            ...update, [e.target.name]: e.target.value
+        })
+    }
+
     return (
-        <div>
-            <form>
+        <div className='formContainer'>
+            <form className='formBox'>
                 <input 
                 type='text'
+                name='title'
+                placeholder='Movie Title'
+                onChange={handleChanges}
                 />
+                <input 
+                type='text'
+                name='director'
+                placeholder='Director'
+                onChange={handleChanges}
+                />
+                <input 
+                type='text'
+                name='metascore'
+                placeholder='MetaScore'
+                onChange={handleChanges}
+                />
+                <input 
+                type='text'
+                name='stars'
+                placeholder='Actors'
+                onChange={handleChanges}
+                />            
             </form>
         </div>
     )
